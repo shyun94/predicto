871d79a11b0ffd575b7dd809fbf07a8cefb735ae
SanghyunLee <michin1559@naver.com>
2024-12-20 10:23:24 +0900

    refactor: typing Foo

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,15 +1,21 @@
 "use client";
 import { useShape } from "@electric-sql/react";
 
+type Foo = {
+  id: number;
+  name: string;
+  value: number;
+};
+
 function Component() {
-  const { data } = useShape({
+  const { data } = useShape<Foo>({
     url: `http://localhost:3000/shape-proxy`,
     params: {
       table: `foo`,
     },
   });
 
-  return <pre>{JSON.stringify(data, null, 2)}</pre>;
+  return <div>{data?.map((foo) => <div key={foo.id}>{foo.name}</div>)}</div>;
 }
 
 export default Component;
