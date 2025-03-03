5e26cba765bb936f7e48f5748aebfbb560c59b73
SanghyunLee <michin1559@naver.com>
2025-03-03 10:46:56 +0900

    feat: 내 원래 프로젝트로 되돌리기

--- apps/vision-pocket/app/page.tsx
+++ apps/vision-pocket/app/page.tsx
@@ -1,157 +1,126 @@
 "use client";
 
-import { AppShell, Button, Flex, Space, Stack } from "@mantine/core";
-import { useState } from "react";
-import { RecurringDeposit, Stock, TimeDeposit } from "@repo/fin-predict";
-import StockForm from "../components/StockForm";
-import { sum } from "es-toolkit";
-import getExpectedStock from "../../../domains/fin-predict/src/calculator/getExpectedStock";
-import formatKoreanCurrency from "../utils/formatKoreanCurrency";
+import { AppShell, Flex } from "@mantine/core";
 import {
-  BarChart as RechartsBarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
+  FinDriver,
+  FinFormulaTableManager,
+  FinModel,
+  FinRow,
+} from "@repo/fin-core";
+import { v4 as uuidv4 } from "uuid";
+import { useEffect, useMemo, useState } from "react";
 
 export default function Page() {
-  const [stock, setStock] = useState<Partial<Stock>>({});
+  const [model, setModel] = useState<FinModel>({
+    id: uuidv4(),
+    name: "",
+    timeRange: ["2025-01", "2025-12"],
+  });
+  const [drivers, setDrivers] = useState<FinDriver[]>([]);
+  const [rows, setRows] = useState<FinRow[]>([]);
 
-  const result = isStockNotPartial(stock)
-    ? [
-        { label: "1년후", ...getExpectedStock({ stock, afterYear: 1 }) },
-        { label: "2년후", ...getExpectedStock({ stock, afterYear: 2 }) },
-        { label: "3년후", ...getExpectedStock({ stock, afterYear: 3 }) },
-        { label: "5년후", ...getExpectedStock({ stock, afterYear: 5 }) },
-        { label: "10년후", ...getExpectedStock({ stock, afterYear: 10 }) },
-        { label: "15년후", ...getExpectedStock({ stock, afterYear: 15 }) },
-        { label: "20년후", ...getExpectedStock({ stock, afterYear: 20 }) },
-        { label: "25년후", ...getExpectedStock({ stock, afterYear: 25 }) },
-        { label: "30년후", ...getExpectedStock({ stock, afterYear: 30 }) },
-        { label: "35년후", ...getExpectedStock({ stock, afterYear: 35 }) },
-        { label: "40년후", ...getExpectedStock({ stock, afterYear: 40 }) },
-      ]
-    : undefined;
+  const [cellValues, setCellValues] = useState<string[][]>([]);
+
+  const finFormulaManager = useMemo(() => {
+    return new FinFormulaTableManager(model);
+  }, [model]);
+
+  useEffect(() => {
+    finFormulaManager.setFinContents({
+      rows,
+      drivers,
+    });
+
+    setCellValues(finFormulaManager.getCellValues());
+    // setAppliedTimeSegmentIds(finFormulaManager.getAppliedTimeSegmentIds(rows));
+  }, [drivers, finFormulaManager, rows]);
 
   return (
-    <AppShell padding="md">
+    <AppShell>
       <AppShell.Main>
-        <Flex justify={"center"} direction={"column"}>
-          <Flex maw={736} direction={"column"} w={"100%"}>
-            <Space h="md" />
+        <Flex direction="column" gap="md">
+          <div>
+            <button
+              onClick={() =>
+                setDrivers((prev) => [
+                  ...prev,
+                  {
+                    id: uuidv4(),
+                    name: "",
+                    value: Math.random().toString(),
+                    format: { type: "number" },
+                    modelId: model.id,
+                    order: prev.length,
+                  },
+                ])
+              }
+            >
+              add driver
+            </button>
+            <button
+              onClick={() =>
+                setRows((prev) => [
+                  ...prev,
+                  makeRandomRow(drivers, model.id, prev.length),
+                ])
+              }
+            >
+              add row
+            </button>
+          </div>
 
-            <Stack>
-              <StockForm onChange={setStock} />
-            </Stack>
-          </Flex>
-          <Space h="md" />
+          <div>
+            model
+            <pre>{JSON.stringify(model, null, 2)}</pre>
+          </div>
 
-          <Button
-            pos="fixed"
-            bottom={0}
-            left={0}
-            right={0}
-            h={60}
-            style={{
-              zIndex: 1000,
-              paddingBottom: "env(safe-area-inset-bottom)",
-            }}
-            size="xl"
-            color="dark.6"
-          >
-            계산하기
-          </Button>
+          <div>
+            drivers
+            <pre>{JSON.stringify(drivers, null, 2)}</pre>
+          </div>
 
-          {result && (
-            <>
-              <Flex maw={736} direction={"column"} w={"100%"}>
-                {result.map((n) => (
-                  <div key={n.label}>
-                    {n.label}: {formatKoreanCurrency(n.total)}
-                  </div>
-                ))}
-                <Space h="md" />
-                <ResponsiveContainer width="100%" height={300}>
-                  <RechartsBarChart data={result}>
-                    <XAxis dataKey="label" />
-                    <YAxis
-                      tickFormatter={(value) => formatKoreanCurrency(value)}
-                    />
-                    <Tooltip content={<CustomTooltip />} />
-                    <Legend />
-                    <Bar
-                      dataKey="initialInvestmentAmount"
-                      fill="#1f78b4"
-                      name="초기 투자금"
-                      stackId="a"
-                    />
-                    <Bar
-                      dataKey="additionalInvestmentAmount"
-                      fill="#e63946"
-                      name="추가 투자금"
-                      stackId="a"
-                    />
-                    <Bar
-                      dataKey="totalProfit"
-                      fill="#2a9d8f"
-                      name="총 수익"
-                      stackId="a"
-                    />
-                  </RechartsBarChart>
-                </ResponsiveContainer>
-              </Flex>
-            </>
-          )}
+          <div>
+            rows
+            <pre>{JSON.stringify(rows, null, 2)}</pre>
+          </div>
+
+          <div>
+            cell values
+            <pre>{JSON.stringify(cellValues, null, 2)}</pre>
+          </div>
         </Flex>
       </AppShell.Main>
     </AppShell>
   );
 }
 
-const isStockNotPartial = (stock: Partial<Stock>): stock is Stock => {
-  if (
-    stock.id &&
-    stock.amount !== undefined &&
-    stock.interestRate !== undefined &&
-    stock.recurringAmount !== undefined
-  ) {
-    return true;
-  }
-  return false;
-};
-
-// 커스텀 툴팁 컴포넌트 정의
-const CustomTooltip: React.FC<{
-  active?: boolean;
-  payload?: any[];
-  label?: string;
-}> = ({ active, payload, label }) => {
-  if (active && payload && payload.length) {
-    const total = payload.reduce((acc, curr) => acc + (curr.value || 0), 0);
-    return (
-      <div
-        style={{
-          background: "white",
-          border: "1px solid #ccc",
-          padding: "10px",
-        }}
-      >
-        <p>
-          <strong>{label}</strong>
-        </p>
-        {payload.map((item) => (
-          <p key={item.name} style={{ color: item.color }}>
-            {item.name}: {formatKoreanCurrency(item.value)} (
-            {Math.round((item.value / total) * 100)}%)
-          </p>
-        ))}
-        <hr />
-        <p>총 합계: {formatKoreanCurrency(total)}</p>
-      </div>
-    );
-  }
-  return null;
+const makeRandomRow = (
+  drivers: FinDriver[],
+  modelId: string,
+  index: number
+): FinRow => {
+  const id = uuidv4();
+  return {
+    id,
+    name: "",
+    order: index,
+    format: { type: "number" },
+    modelId: modelId,
+    timeSegments: [
+      {
+        id: uuidv4(),
+        formula: "1",
+        timeRange: ["2025-01", "2025-12"],
+        rowId: id,
+        order: 0,
+      },
+      {
+        id: uuidv4(),
+        formula: drivers[0] ? `={{${drivers[0]?.id}}} + {{${id},x-1}}` : "1",
+        timeRange: ["2025-02", "2025-12"],
+        rowId: id,
+        order: 1,
+      },
+    ],
+  };
 };
